7751555185af51cae8ed7b3d831407a6bdead78f
sahardeldar <shrdeldar@gmail.com>
2022-07-16 19:30:03 +0430

    update

--- snake.py
+++ snake.py
@@ -1,9 +1,9 @@
 t=int(input("enter the number :"))
 for i in range(t):
     if i%2==0:
-        print("$",end="")
+        print("*",end="")
     else:
         print("#",end="")
   
-    print("")
+  
     
